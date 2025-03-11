c54d8177a1d9d8e227f7ff42957a3418cdb9a9c4
Sri chandana Gowtham Nukala <srichandanagowthamnukala@Sris-MacBook-Air.local>
2025-03-11 14:45:11 -0400

    Updated backend with expense API

--- app.py
+++ app.py
@@ -60,7 +60,8 @@ def get_expenses():
         for e in expenses
     ])
 
-# Run the app
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
 
+
+@app.route("/")
+def home():
+    return "Welcome to Personal Finance Tracker API!"
